64c28b972291a01394075499e089349dcb362a04
hanassabio <34634608+hanassabio@users.noreply.github.com>
2019-02-20 20:50:29 +0000

    Update publish.py

--- RaspberryPi/publish.py
+++ RaspberryPi/publish.py
@@ -27,32 +27,35 @@ myMQTTClient.connect()
 
 
 ##################Sensor Set-Up####################
-bus = smbus.SMBus(1)
-bus2 = smbus.SMBus(1)
+bus = smbus.SMBus(1)                #bus for air quality sensor
+bus2 = smbus.SMBus(1)		    #bus2 for temp and humid sensor
+#printing initial status
 print("Initial Status: ", bus.read_i2c_block_data(0x5b,0x00,1))
 emptylist = []
 bus.write_i2c_block_data(0x5b, 0xf4,emptylist)
 print("Written the empty list to 0xf4")
 print("Status is now: ", bus.read_i2c_block_data(0x5b,0x00,1))
 print("ErrorID is: ", bus.read_i2c_block_data(0x5b, 0xe0,1))
+#setting mode to be 100
 mode = [0b100]
 mode[0]=mode[0]<<4
 bus.write_i2c_block_data(0x5b,0x01,mode)
 
 
 while True:
-	bus2.write_byte(0x40,0xF5)
+	bus2.write_byte(0x40,0xF5)        #0xF5 is the command for taking measurements for humidity
 	time.sleep(0.25)
-	data0 = bus2.read_byte(0x40)
-	data1 = bus2.read_byte(0x40)
-	humidity = ((data0*256+data1)*125/65536.0)
+	data0 = bus2.read_byte(0x40)       #reading MS-Byte of data
+	data1 = bus2.read_byte(0x40)       #reading LS-Byte of data
+	humidity = ((data0*256+data1)*125/65536.0)      #conversion of data into meaningful values(calculation given by datasheet)
 
-	bus2.write_byte(0x40,0xF3)
+	bus2.write_byte(0x40,0xF3)         #0xF3 is the command for taking measurements for temperature
 	time.sleep(0.65)
-	data0 = bus2.read_byte(0x40)
-	data1 = bus2.read_byte(0x40)
-	temperature = ((data0*256+data1)*175.72/65536.0)-46.85-6	
+	data0 = bus2.read_byte(0x40)        #reading MS-Byte of data
+	data1 = bus2.read_byte(0x40)        #reading LS-Byte of data
+	temperature = ((data0*256+data1)*175.72/65536.0)-46.85        #conversion of data into meaningful values(calculation given by datasheet)	
 	
+	#data01 and data23 are humidity and temperature values used for compensating the air quality sensor
 	data01 = round(humidity/512)
 
 	tdata=temperature
@@ -62,15 +65,19 @@ while True:
 		tdata=-25
 	data23 = round((tdata+25)*2.55)
 	
+	#writing humidity and temperature values onto ENV_DATA address
 	bus.write_i2c_block_data(0x5b,0x05,[data01,0x00,data23,0x00])
-
+	
+        #reading 4 bytes of data
 	data = bus.read_i2c_block_data(0x5b,0x02,4)
-	co2data = (data[0]<<8)+data[1]
-	TVOCdata = (data[2]<<8)+data[3]
+	co2data = (data[0]<<8)+data[1]         #MS two bytes are co2 data
+	TVOCdata = (data[2]<<8)+data[3]        #LS two bytes are tvoc data
+	
 	humid = float("%.1f" % humidity)
 	temp = float("%.1f" % temperature)
 	current_time = time.time()
-
+	
+	#publishing json data onto topic
 	jsonData =json.dumps({"time":current_time,"co2":co2data,"TVOC":TVOCdata, "humidity":humid, "temperature":temp})
 	
 	myMQTTClient.publish(topic,jsonData,1)
